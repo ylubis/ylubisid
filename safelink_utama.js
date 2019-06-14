8a7761e4c1da273a2cea2ce3ce9aa9d7b43e35ee
ylubis <yusufgntnx@gmail.com>
2019-06-14 14:53:52 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -19,12 +19,9 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*1)+ 1);
 var xxx=null;
-if(x=="1"){xxx="link-1.html"}
-if(x=="2"){xxx="link-2.html"}
-if(x=="3"){xxx="link-3.html"}
-if(x=="4"){xxx="link-4.html"}a_to_ve[i].href="https://ylubisid.blogspot.com/p/url.html"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="link-1.html"}a_to_ve[i].href="https://ylubisid.blogspot.com/p/url.html"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
