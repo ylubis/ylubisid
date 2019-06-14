fb16a22c000d994acd9928dabe0b2d66fd3a3c86
ylubis <yusufgntnx@gmail.com>
2019-06-14 14:58:52 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -21,7 +21,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*1)+ 1);
 var xxx=null;
-if(x=="1"){xxx="link-1.html"}a_to_ve[i].href="https://ylubisid.blogspot.com/p/url.html"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="go.html"}a_to_ve[i].href="https://ylubisid.blogspot.com/2019/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
